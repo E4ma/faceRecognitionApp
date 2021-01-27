6a21de121e8fbf233931024e7de76e078c142d5f
Ifeoma <ifeomaidoko@gmail.com>
2021-01-27 09:45:26 -0700

    Route to increase entries per image

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'fb506370a709416cab9fd9496cf66b44'
+ apiKey: 'YOUR API KEY HERE'
 });
 
 const particlesOptions = {
@@ -46,6 +46,15 @@ class App extends Component {
     }
   }
 
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -81,9 +90,27 @@ class App extends Component {
         // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
         // to:
         // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-        'c0c0ac362b03416da06ab3fa36fb58e3',
+        Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              //Object.assign updates only certain element in user object
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -107,7 +134,10 @@ class App extends Component {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
@@ -116,8 +146,8 @@ class App extends Component {
             </div>
           : (
              route === 'signin'
-             ? <Signin onRouteChange={this.onRouteChange}/>
-             : <Register onRouteChange={this.onRouteChange}/>
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
